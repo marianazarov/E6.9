5f19d7061cc50e8d4adfacd98f9a1879f23e939d
Мария Назарова <134504788+marianazarov@users.noreply.github.com>
2024-02-06 03:05:37 +1200

    Update asgi.py

--- messenger/asgi.py
+++ messenger/asgi.py
@@ -4,7 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from messenger.messenger_server.routing import ws_urlpatterns
+from messenger_server.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
 
